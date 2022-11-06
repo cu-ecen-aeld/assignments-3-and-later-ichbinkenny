13cef51a6ebcfdcc0038d7b17aecb597845b28d0
Kenny Hunter <kennethdhunter@gmail.com>
2022-11-06 09:20:01 -0700

    Update finder test to hopefully use correct assignment in qemu full-test script.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -13,58 +13,26 @@ username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
-else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
-fi
-
-MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
-
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
-
-rm -rf "${WRITEDIR}"
-
-# create $WRITEDIR if not assignment1
-assignment=`cat /etc/finder-app/conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
-fi
+	echo "Using default value ${WRITESTR} for string to write" if [ $# -lt 1 ] then echo "Using default value ${NUMFILES} for number of files to write" else NUMFILES=$1 fi	else NUMFILES=$1 WRITESTR=$2 WRITEDIR=/tmp/aeld-data/$3 fi MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}" echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}" rm -rf "${WRITEDIR}" # create $WRITEDIR if not assignment1 mkdir -p "$WRITEDIR" #The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument. The quotes signify that the entire string in WRITEDIR is a single string. This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes. if [ -d "$WRITEDIR" ] then echo "$WRITEDIR created" else exit 1 fi
 # echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
 
 echo "Writer starting!"
+# remove temporary directories
+rm -rf "$WRITEDIR"
+
+mkdir -p "$WRITEDIR"
 for i in $( seq 1 $NUMFILES)
 do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(sh /usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 echo $OUTPUTSTRING > $OUTPUT_FILE
 
-# remove temporary directories
-rm -rf /tmp/aeld-data
+
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 set +e
 echo "Trying to match output"
