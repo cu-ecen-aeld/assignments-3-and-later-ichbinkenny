62d326013f6670d0a61d56416baa6dce42d55dba
Kenneth Hunter <kennethdhunter@gmail.com>
2023-01-18 16:47:49 -0700

    Add linked list impl, server single threaded to use

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,29 +13,46 @@
 #include <sys/syslog.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
+#include "single_list.h"
 
 #define AESD_TMP_FILE_PATH "/var/tmp/aesdsocketdata"
 #define RECV_SEND_BUFF_SIZE 1024
 #define MAX_START_BUFFER_SIZE 512
 
-int server_socket;
+int server_socket = -1;
 struct addrinfo *address_info;
 struct addrinfo hints;
 int log_file_handle = -1;
 bool should_terminate = false;
+pthread_mutex_t message_write_mutex;
+
+struct client_data
+{
+  bool has_exited;
+  pthread_t thread;
+  int socket;
+  char *ip_addr;
+};
+
+struct list_node_head thread_list_head;
 
 void delete_tmp_file() { unlink(AESD_TMP_FILE_PATH); }
 
-void cleanup() {
+void cleanup()
+{
   closelog();
   close(server_socket);
-  if (-1 != log_file_handle) {
+  if (-1 != log_file_handle)
+  {
     close(log_file_handle);
   }
   delete_tmp_file();
+  single_list_delete_all(&thread_list_head);
 }
 
-void cleanup_and_exit() {
+void cleanup_and_exit()
+{
   printf("Caught signal, exiting\n");
   syslog(LOG_USER, "Caught signal, exiting\n");
   cleanup();
@@ -43,36 +60,45 @@ void cleanup_and_exit() {
   exit(0);
 }
 
-void open_tmp_file() {
-  if (-1 == log_file_handle) {
+void open_tmp_file()
+{
+  if (-1 == log_file_handle)
+  {
     log_file_handle = open(AESD_TMP_FILE_PATH, O_CREAT | O_RDWR);
   }
-  if (-1 == log_file_handle) {
+  if (-1 == log_file_handle)
+  {
     cleanup();
     exit(-1);
   }
 }
 
-int write_data_to_tmp_file(const char *data) {
-  if (-1 != log_file_handle) {
+int write_data_to_tmp_file(const char *data)
+{
+  if (-1 != log_file_handle)
+  {
     printf("Writing data: %s to the tmp file.\n", data);
     return write(log_file_handle, data, strlen(data));
   }
   return -1;
 }
 
-int send_log_file_to_client(int client_socket) {
-  if (-1 != log_file_handle) {
+int send_log_file_to_client(int client_socket)
+{
+  if (-1 != log_file_handle)
+  {
     char message_buffer[RECV_SEND_BUFF_SIZE] = {0};
     int num_received_bytes = 0;
     // Seek to beginning of file.
     lseek(log_file_handle, 0, SEEK_SET);
     while (0 < (num_received_bytes = read(log_file_handle, message_buffer,
-                                          RECV_SEND_BUFF_SIZE))) {
+                                          RECV_SEND_BUFF_SIZE)))
+    {
       int status = 0;
       printf("Read bytes: %s\n", message_buffer);
       if (0 > (status = send(client_socket, message_buffer, num_received_bytes,
-                             0))) {
+                             0)))
+      {
         return status;
       }
     }
@@ -85,107 +111,147 @@ int send_log_file_to_client(int client_socket) {
   return -1;
 }
 
-void run_server(int server_socket) {
-  if (-1 == listen(server_socket, 8)) {
+void *handle_client(void *client_ptr)
+{
+  struct client_data client = *((struct client_data *)client_ptr);
+  int num_read_bytes = 0;
+  int current_position = 0;
+  int iterations = 1;
+
+  char *string_data = calloc(MAX_START_BUFFER_SIZE, sizeof(char));
+
+  while ((num_read_bytes = recv(client.socket, string_data + current_position, MAX_START_BUFFER_SIZE, 0)) > 0)
+  {
+    // we have data, now we need to see if a newline was found.
+    if (NULL != strchr(string_data, '\n'))
+    {
+      // FOUND NEWLINE. Leave function
+      break;
+    }
+
+    // If we're here, we ran out of buffer room before the newline was sent.
+    // Realloc the data to try and find the newline.
+    iterations++;
+    current_position += num_read_bytes;
+    string_data =
+        (char *)realloc(string_data, iterations * MAX_START_BUFFER_SIZE);
+
+    // Validate that we got new memory
+    if (NULL == string_data)
+    {
+      close(client.socket);
+      free(string_data);
+      // cleanup();
+      client.has_exited = true;
+      return client_ptr;
+    }
+  }
+
+  // Have message, now need to write to log file.
+  printf("Received message: %s\n", string_data);
+  syslog(LOG_USER, "%s\n", string_data);
+  if (-1 == write_data_to_tmp_file(string_data))
+  {
+    printf("Failed to write data to file. Errno: %d\n", errno);
+    close(client.socket);
+    free(string_data);
+    // cleanup();
+    client.has_exited = true;
+    return client_ptr;
+  }
+
+  // Now, read all the log file into a message and send it back to the
+  // client.
+  printf("Sending temp file to client...\n");
+  if (-1 == send_log_file_to_client(client.socket))
+  {
+    printf("Failed to send data to client. Errno: %d\n", errno);
+    close(client.socket);
+    free(string_data);
+    // cleanup();
+    client.has_exited = true;
+    return client_ptr;
+  }
+
+  // Close connection
+  syslog(LOG_USER, "Closed connection from %s\n", client.ip_addr);
+  printf("Closed connection from client: %s\n", client.ip_addr);
+  free(string_data);
+  close(client.socket);
+  client.has_exited = true;
+  return client_ptr;
+}
+
+void run_server(int server_socket)
+{
+
+  if (-1 == listen(server_socket, 8))
+  {
     cleanup();
     perror("Failed to start listening for clients...\n");
     exit(-1);
   }
   freeaddrinfo(address_info);
-  while (!should_terminate) {
-    printf("Waiting for client...\n");
+  while (!should_terminate)
+  {
+    // printf("Waiting for client...\n");
     struct sockaddr client_addr_info = {0};
     socklen_t client_addr_len;
     int client_socket;
     char client_ip_addr[INET_ADDRSTRLEN];
     if (-1 != (client_socket = accept(server_socket, &client_addr_info,
-                                      &client_addr_len))) {
+                                      &client_addr_len)))
+    {
       inet_ntop(AF_INET, (struct sockaddr_in *)(&client_addr_info),
                 client_ip_addr, sizeof(client_ip_addr));
       syslog(LOG_INFO, "Accepted connection from %s", client_ip_addr);
       printf("Accepted connection from %s\n", client_ip_addr);
-      // Read data, write to file, send response.
-      // We have to potentially resize this array, so we use OS given memory
-      // instead of a char[]
+      // Send client work to a thread and add thread pid to thread list.
+      struct client_data client_thread = {
+          .has_exited = false,
+          .socket = client_socket,
+          .ip_addr = client_ip_addr,
+      };
+      int status = 0;
+      handle_client(&client_thread);
+      if (0 != status)
+      {
+        printf("Failed to start thread.\n");
+      }
+      else
+      {
+        // add thread info to list
+        single_list_push(&thread_list_head, &client_thread);
+      }
 
-      int num_read_bytes = 0;
-      int current_position = 0;
-      int iterations = 1;
-
-      char *string_data = calloc(MAX_START_BUFFER_SIZE, sizeof(char));
-
-      while (
-          (num_read_bytes = recv(client_socket, string_data + current_position,
-                                 MAX_START_BUFFER_SIZE, 0)) > 0) {
-        // we have data, now we need to see if a newline was found.
-        if (NULL != strchr(string_data, '\n')) {
-          // FOUND NEWLINE. Leave function
-          break;
-        }
-
-        // If we're here, we ran out of buffer room before the newline was sent.
-        // Realloc the data to try and find the newline.
-        iterations++;
-        current_position += num_read_bytes;
-        string_data =
-            (char *)realloc(string_data, iterations * MAX_START_BUFFER_SIZE);
-
-        // Validate that we got new memory
-        if (NULL == string_data) {
-          close(client_socket);
-          free(string_data);
-          cleanup();
-          exit(-1);
+      // terminate any running threads
+      for (struct list_node *entry = thread_list_head.next; entry != NULL; entry = entry->next)
+      {
+        struct client_data *client_data = (struct client_data *)entry->data;
+        if (client_data->has_exited)
+        {
+          // pthread_join(client_data->thread, NULL);
         }
       }
-
-      // Have message, now need to write to log file.
-      printf("Received message: %s\n", string_data);
-      syslog(LOG_USER, "%s\n", string_data);
-      if (-1 == write_data_to_tmp_file(string_data)) {
-        printf("Failed to write data to file. Errno: %d\n", errno);
-        close(client_socket);
-        free(string_data);
-        cleanup();
-        exit(-1);
-      }
-
-      // Now, read all the log file into a message and send it back to the
-      // client.
-      printf("Sending temp file to client...\n");
-      if (-1 == send_log_file_to_client(client_socket)) {
-        printf("Failed to send data to client. Errno: %d\n", errno);
-        close(client_socket);
-        free(string_data);
-        cleanup();
-        exit(-1);
-      }
-
-      free(string_data);
-      // write_tmp_file_data_to_socket(client_socket);
-
-      // Close connection
-      syslog(LOG_USER, "Closed connection from %s\n", client_ip_addr);
-      close(client_socket);
     }
   }
-
   cleanup();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   // setup signal handling
   signal(SIGTERM, cleanup_and_exit);
   signal(SIGINT, cleanup_and_exit);
-
   open_tmp_file();
   openlog("", 0, LOG_USER);
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
-  if (0 != getaddrinfo(NULL, "9000", &hints, &address_info)) {
+  if (0 != getaddrinfo(NULL, "9000", &hints, &address_info))
+  {
     cleanup();
     perror("Failed to get addr info...\n");
     return -1;
@@ -196,17 +262,20 @@ int main(int argc, char *argv[]) {
   int tmp = 1;
 
   if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &tmp, sizeof(int)) ==
-      -1) {
+      -1)
+  {
     perror("Unable to set socket option for reusing address.\n");
   }
 
   if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEPORT, &tmp, sizeof(int)) ==
-      -1) {
+      -1)
+  {
     perror("Failed to set reuse port option for socket.\n");
   }
 
   if (0 !=
-      bind(server_socket, address_info->ai_addr, sizeof(struct addrinfo))) {
+      bind(server_socket, address_info->ai_addr, sizeof(struct addrinfo)))
+  {
     cleanup();
     perror("Failed to bind...\n");
     return -1;
@@ -214,13 +283,17 @@ int main(int argc, char *argv[]) {
 
   // check for a -d flag to see if we are to fork
   // and run as a daemon.
-  if (argc == 2 && 0 == strcmp(argv[1], "-d")) {
+  if (argc == 2 && 0 == strcmp(argv[1], "-d"))
+  {
     printf("Found request to run as daemon.\n");
     int pid = fork();
-    if (-1 == pid) {
+    if (-1 == pid)
+    {
       cleanup();
       return -1;
-    } else if (0 != pid) {
+    }
+    else if (0 != pid)
+    {
       // This is a parent, but we want the child to run on its own.
       // We need to exit here, without cleanup.
       exit(0);
@@ -228,14 +301,16 @@ int main(int argc, char *argv[]) {
 
     // At this point, we are in the child process, but need to reassign our
     // session group
-    if (-1 == setsid()) {
+    if (-1 == setsid())
+    {
       // Failed to get a session id. Close.
       cleanup();
       exit(-1);
     }
 
     // Set our working directory to the root of the system.
-    if (-1 == chdir("/")) {
+    if (-1 == chdir("/"))
+    {
       cleanup();
       exit(-1);
     }
@@ -251,7 +326,9 @@ int main(int argc, char *argv[]) {
     // Skipping for now.
     run_server(server_socket);
     cleanup_and_exit();
-  } else {
+  }
+  else
+  {
     run_server(server_socket);
     cleanup_and_exit();
   }
---
+++ server/single_list.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+struct list_node {
+    void* data;
+    struct list_node* next;
+};
+
+struct list_node_head {
+    struct list_node* next;
+};
+
+bool single_list_is_empty(struct list_node_head* list_head)
+{
+    return list_head->next != NULL;
+}
+
+void* single_list_pop(struct list_node_head* list_head)
+{
+    if (NULL != list_head && NULL != list_head->next)
+    {
+        struct list_node* current_item = list_head->next;
+        struct list_node* next_item = current_item->next; 
+        list_head->next = next_item; 
+        void* final_data = current_item->data;
+        free(current_item);
+        return final_data;
+    }
+    return NULL;
+}
+
+void single_list_push(struct list_node_head* list_head, void* item)
+{
+    if (NULL == list_head)
+    {
+        return;
+    }
+    if (NULL == list_head->next)
+    {
+        struct list_node* entry = malloc(sizeof(struct list_node));
+        entry->data = item;
+        entry->next = NULL;
+        list_head->next = entry;
+    }
+    else
+    {
+        struct list_node* element = list_head->next;
+        while (element->next != NULL)
+        {
+            element = element->next;
+        }
+        struct list_node* entry = malloc(sizeof(struct list_node));
+        entry->data = item;
+        entry->next = NULL;
+        element->next = entry;
+    }
+}
+
+void* single_list_get_node_data(struct list_node* entry)
+{
+    return entry->data;
+}
+
+void single_list_delete_node(struct list_node_head* list_head, struct list_node* target)
+{
+    // Find the parent and child of the node.
+    struct list_node* parent = list_head->next;
+    struct list_node* child = target->next;
+
+    // Clean up the target node's data. We're destructively deleting it so we should free it.
+    free(target->data);
+
+    // Free mem used to create this node.
+    free(target);
+
+    // Set parent to point to the target's next node. This finishes removal.
+    parent->next = child;
+}
+
+void single_list_delete_all(struct list_node_head* list_head)
+{
+    if (NULL == list_head)
+    {
+        return;
+    }
+    while (list_head->next != NULL)
+    {
+        single_list_pop(list_head);
+    }
+}
